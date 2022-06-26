eddd779f0007ad3469135cb725bc86f57241bf19
Hasan Basri Aygümüş <hbasri2304@gmail.com>
2022-06-26 23:25:23 +0300

    Create sample01.cpp

---
+++ Basic Data Types/sample01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    double d;
+    float f;
+    
+    cin >> a >> b>> c >> d >> f;
+    
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<f<<endl;
+    
+    return 0;
+}
